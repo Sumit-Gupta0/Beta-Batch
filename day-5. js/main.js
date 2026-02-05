d00bd2a73fa4eab5107f2f593239a75fe3069cc6
Sumit-Gupta0 <sumitgupta86400@gmail.com>
2026-02-05 17:51:47 +0530

    First Commit

--- day-5. js/main.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById("btn")
-const head = document.getElementById("head")
-
-let num = 0;
-btn.addEventListener('click', () => {
-    num++;
-    head.innerHTML = num;
-})
